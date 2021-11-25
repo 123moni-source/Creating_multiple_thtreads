2a606a5605886982f155afff989ac2aa2568075d
MONISHKA SINGH <singhmonishkass@gmail.com>
2021-11-25 23:42:33 +0530

    Initial commit

---
+++ src/MultiThreadDemo.java
@@ -0,0 +1,36 @@
+//create multiple threads
+class NewThread implements Runnable{
+        String name;
+        Thread t;
+
+        NewThread(String threadname){
+            name=threadname;
+            t=new Thread(this,name);
+            System.out.println("New thread:"+t);
+            t.start();
+        }
+        public void run(){
+            try{
+                for(int i=5;i>0;i--) {
+                    System.out.println(name + ":" + i);
+                    Thread.sleep(1000);
+                }
+            }catch(InterruptedException e){
+                System.out.println(name+"Interrupted");
+            }
+            System.out.println(name+"exiting");
+        }
+}
+public class MultiThreadDemo {
+    public static void main(String[] args) {
+        new NewThread("one");
+        new NewThread("two");
+        new NewThread("three");
+        try{
+            Thread.sleep(10000);
+        }catch(InterruptedException e){
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("main thread exiting");
+    }
+}
